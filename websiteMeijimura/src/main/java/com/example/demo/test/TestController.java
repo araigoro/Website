aebd567d1bc881c2dce1024399d09d7216af37a8
DESKTOP-EDJF9MH\kaito <kaito1019baseball@icloud.com>
2023-09-02 18:28:37 +0900

    ・テスト用html、cssにヘッダーのロゴ作成

--- websiteMeijimura/src/main/java/com/example/demo/test/TestController.java
+++ websiteMeijimura/src/main/java/com/example/demo/test/TestController.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloController {
-	
+public class TestController {
+
 	@GetMapping("/hello")
 	public String getHello() {
 		return "test/hello";
+	}
+
+	@GetMapping("/test")
+	public String getTest() {
+		return "test/test";
 	}
 }
